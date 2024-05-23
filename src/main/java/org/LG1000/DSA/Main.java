2e22e764a51eaba0a2f703a20a15be1ed52026a8
lovegupta0 <lovegupta307@gmail.com>
2024-05-23 19:38:33 +0530

    Added BinaryTree and its derivative

--- src/main/java/org/LG1000/DSA/Main.java
+++ src/main/java/org/LG1000/DSA/Main.java
@@ -1,26 +1,20 @@
 package org.LG1000.DSA;
 
 
-import org.LG1000.DSA.LinkedList.DoubleLinkedList.DoubleLinkedListOp;
-
-import org.LG1000.DSA.LinkedList.SingleLinkedList.SingleLinkedListOp;
+import org.LG1000.DSA.PriorityQueue.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) {
-       DoubleLinkedListOp list = new DoubleLinkedListOp();
-       list.insert(10); // Insertion at the end
-       list.insertAtBeginning(5); // Insertion at the beginning
-       list.insertAtPosition(7, 2); // Insertion at position 2
-       list.insert(15); // Insertion at the end
-       list.insert(20); // Insertion at the end
-       list.display(); // Display the list: 5 7 10 15 20
+       PriorityQueue<Integer> pq = new PriorityQueue<>();
+       pq.add(3);
+       pq.add(1);
+       pq.add(4);
+       pq.add(2);
 
-       list.deleteAtBeginning(); // Deletion at the beginning
-       list.deleteAtEnd(); // Deletion at the end
-       list.deleteAtPosition(2); // Deletion at position 2
-       list.display(); // Display the list after deletions: 7 15
+       System.out.println("Peek: " + pq.peek()); // Peek: 1
 
-       list.deleteValue(7); // Deletion of value 7
-       list.display(); // Display the list after deletion of value 7: 15
+       while (!pq.isEmpty()) {
+          System.out.println("Poll: " + pq.poll()); // Poll: 1, Poll: 2, Poll: 3, Poll: 4
+       }
     }
 }
